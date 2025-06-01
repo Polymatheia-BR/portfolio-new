638ba21c5dd2f2c56fcaf2fd24d3d84399cad958
Victor Ferreira França <victorferreira8613@gmail.com>
2025-06-01 02:23:00 -0300

    Minor bug fix

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -14,7 +14,7 @@ export default function MainBody() {
                     <Text fw={700} size={70} lh="1" className="titleText">FULLSTACK <span style={{color:'var(--mantine-color-blue-7)'}}><br/>DEVELOPER</span></Text>
                     <Text w={500}><Text span fw={600} c="blue.7">Olá! Seja bem vindo...</Text><br/> Eu me chamo Victor Ferreira e sou um desenvolvedor e analista de sistemas apaixonado por computação!</Text>
                 </Stack>    
-                <Image src="/src/assets/dev_avatar.svg?react" w={600} />                
+                <Image src="/dev_avatar.svg?react" w={600} />                
             </Group>
         </motion.div>   
     )
